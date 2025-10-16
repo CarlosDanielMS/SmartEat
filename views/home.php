9f9dc03a7afe0c00296ba75494dbc08b1f822e5f
Carlos Daniel <118182410+CarlosDanielMS@users.noreply.github.com>
2025-10-15 21:04:56 -0300

    Delete views directory

--- views/home.php
+++
@@ -1,5 +0,0 @@
-
-<section class="container-fluid d-flex flex-column justify-content-center align-items-center" style="height: 100vh">
-    <h1 class="text-center">Simples. Sustentável. Estilo de Vida.</h1>
-    <a href="<?=BASE_URL?>quiz/start" class="btn btn-success">INICIAR</a>
-</section>
