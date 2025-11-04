e346cfa6f1548cf2e5f47187bf10df06806b6c0e
Carlos Daniel <118182410+CarlosDanielMS@users.noreply.github.com>
2025-11-04 19:45:05 -0300

    Delete api directory

--- api/src/config.js
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-
-export const CONFIG = {
-  port: process.env.PORT || 4000,
-  accessSecret: process.env.JWT_ACCESS_SECRET,
-  refreshSecret: process.env.JWT_REFRESH_SECRET,
-  accessExpiresIn: process.env.JWT_ACCESS_EXPIRES || '15m',
-  refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES || '7d',
-};
